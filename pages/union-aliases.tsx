48ed3d9e612106c2a22f42a1e1011e7adaf73fe9
Zach Neibaur <zneibaur@protonmail.com>
2022-05-26 16:02:44 -0600

    Function types example

--- pages/union-aliases.tsx
+++ pages/union-aliases.tsx
@@ -11,8 +11,17 @@ const UnionAliases: NextPage = () => {
 
   printResult(add(5, 12))
 
+  // Function types with parameters types and return type
+  let combineValues: (a: number, b: number) => number
+
+  combineValues = add
+  // combineValues = printResult - Won't work because the paramters and return type don't match
+  // combineValues = 5
+
+  console.log(combineValues(8,8))
+
   return (
-    <div>Union Aliases</div>
+    <div>Union Aliases and Function Types</div>
   )
 }
 
