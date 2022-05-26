f8ec051785a514de22463eea408c3714f07153e0
Zach Neibaur <zneibaur@protonmail.com>
2022-05-26 07:56:41 -0600

    Literal types example

---
+++ pages/literal.tsx
@@ -0,0 +1,24 @@
+export default function LiteralTypes() {
+  const combine = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
+    let result
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+      result = +input1 + +input2
+    } else {
+      result = input1.toString() + input2.toString()
+    }
+    return result
+  }
+
+  const combinedAges = combine(30, 26, 'as-number')
+  console.log(combinedAges)
+
+  const combinedStringAges = combine(30, 26, 'as-number')
+  console.log(combinedStringAges)
+
+  const combinedNames = combine('Zach', 'Neibaur', 'as-text')
+  console.log(combinedNames)
+
+  return (
+    <div>Literal Types</div>
+  )
+}
