d6298091130669fb1e1c37fab7d7fde94889be43
Zach Neibaur <zneibaur@protonmail.com>
2022-05-25 22:41:07 -0600

    Union types practice

---
+++ pages/union.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+
+const Union: NextPage = () => {
+  const combine = (input1: number | string, input2: number | string) => {
+    let result
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2
+    } else {
+      result = input1.toString() + input2.toString()
+    }
+    return result
+  }
+
+  const combinedAges = combine(30, 26)
+  console.log(combinedAges)
+
+  const combinedNames = combine('Zach', 'Neibaur')
+  console.log(combinedNames)
+
+  return (
+    <section>Union Types</section>
+  )
+}
+
+export default Union
