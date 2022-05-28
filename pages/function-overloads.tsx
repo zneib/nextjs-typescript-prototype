54247b35325b4bd38bc6e3b77a11a803c842b574
Zach Neibaur <zneibaur@protonmail.com>
2022-05-28 11:51:36 -0600

    Updated function overload example

--- pages/function-overloads.tsx
+++ pages/function-overloads.tsx
@@ -3,12 +3,18 @@ import { NextPage } from "next";
 type Combinable = string | number
 
 const FunctionOverloads: NextPage = () => {
-  const add = (a: Combinable, b: Combinable) => {
+
+  function add(a: number, b: number): number; // This is a function overload
+  function add(a: string, b: string): string; // This is a function overload
+  function add(a: Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string') {
-      return a.toString() + b.toString()
+      return a.toString() + b.toString();
     }
-    return a + b
+    return a + b;
   }
+
+  const result = add('Max', 'Schwarz')
+  result.split(' ')
   return (
     <h1>Function Overloads</h1>
   )
