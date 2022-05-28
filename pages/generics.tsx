c172fe6b129c3669f2cd0e668da123a08c2e859f
Zach Neibaur <zneibaur@protonmail.com>
2022-05-28 12:37:26 -0600

    Beginning generics example

---
+++ pages/generics.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+
+const Generics: NextPage = () => {
+  // const names: Array<string> = [] // same as string[]
+
+  // const promise: Promise<string> = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve('This is done')
+  //   }, 2000);
+  // })
+
+  return (
+    <h1>Generics</h1>
+  )
+}
+
+export default Generics
