60be9514e9782b8aae0a82d6867400a46512c7f9
Zach Neibaur <zneibaur@protonmail.com>
2022-05-29 14:39:37 -0600

    Generic constraints

--- pages/generics.tsx
+++ pages/generics.tsx
@@ -9,11 +9,12 @@ const Generics: NextPage = () => {
   //   }, 2000);
   // })
 
-  function merge(objA: object, objB: object) {
+  function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
   }
 
-  console.log(merge({name: 'Max'}, {age: 30}))
+  const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
+  console.log(mergedObj);
 
   return (
     <h1>Generics</h1>
