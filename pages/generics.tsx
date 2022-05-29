b6c627965c50ecbe3ea5ce8ce19cb03d08c4fbe5
Zach Neibaur <zneibaur@protonmail.com>
2022-05-29 15:42:37 -0600

    Readonly and Partial utility types

--- pages/generics.tsx
+++ pages/generics.tsx
@@ -70,6 +70,25 @@ const Generics: NextPage = () => {
   // objStorage.removeItem({name: 'Max'}); // This will break!
   // console.log(objStorage.getItems())
 
+  interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+  }
+
+  function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {}
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    
+    return courseGoal as CourseGoal;
+  }
+
+  const names: Readonly<string[]> = ['Max', 'Steve'];
+  // names.push('Zach');
+  // names.pop();
+
   return (
     <h1>Generics</h1>
   )
