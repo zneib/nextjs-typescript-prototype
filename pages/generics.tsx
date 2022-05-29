ef32e6092390e41cc0ec2bc44c06f1b220ea0c18
Zach Neibaur <zneibaur@protonmail.com>
2022-05-29 14:51:46 -0600

    Another generic function example

--- pages/generics.tsx
+++ pages/generics.tsx
@@ -16,6 +16,23 @@ const Generics: NextPage = () => {
   const mergedObj = merge({name: 'Max', hobbies: ['Sports']}, {age: 30})
   console.log(mergedObj);
 
+  interface Lengthy {
+    length: number;
+  }
+
+  function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+      descriptionText = `Got 1 element`;
+    } else if (element.length > 1) {
+      descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText]
+  }
+
+  console.log(countAndDescribe(['hi', 'world']));
+  
+
   return (
     <h1>Generics</h1>
   )
