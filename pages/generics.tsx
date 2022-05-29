a360da06df1b567f9325ceaf250c46015f3ed801
Zach Neibaur <zneibaur@protonmail.com>
2022-05-29 15:15:01 -0600

    Generics with classes example

--- pages/generics.tsx
+++ pages/generics.tsx
@@ -37,7 +37,38 @@ const Generics: NextPage = () => {
   }
 
   console.log(extractAndConvert({name: 'Max'}, 'name'))
+
+  class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+      this.data.push(item)
+    }
+
+    removeItem(item: T) {
+      if (this.data.indexOf(item) === 1) {
+        this.data.splice(this.data.indexOf(item, 1))
+      }
+    }
+
+    getItems() {
+      return [...this.data];
+    }
+  }
+
+  const textStorage = new DataStorage<string>();
+  textStorage.addItem('Max');
+  textStorage.addItem('Zach');
+  textStorage.removeItem('Max');
+  console.log(textStorage.getItems())
   
+  const numberStorage = new DataStorage<number>();
+
+  // const objStorage = new DataStorage<object>();
+  // objStorage.addItem({name: 'Max'})
+  // objStorage.addItem({name: 'Zach'})
+  // objStorage.removeItem({name: 'Max'}); // This will break!
+  // console.log(objStorage.getItems())
 
   return (
     <h1>Generics</h1>
